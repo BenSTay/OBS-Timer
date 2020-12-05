f5ff9d0357f0698dd82b38181124fd6d9791234f
Ben Taylor <staylor.ben@gmail.com>
2020-12-04 18:04:09 -0800

    Created a simple countdown timer using Windows Forms that outputs to a text file that can then be read by OBS. Could definitely be improved, but it works well enough.

---
+++ OBS Timer/OBS Timer/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace OBS_Timer
+{
+    partial class Timer_Form
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.clock = new System.Windows.Forms.Timer(this.components);
+            this.clockBox = new System.Windows.Forms.GroupBox();
+            this.clockLabel = new System.Windows.Forms.Label();
+            this.timerBox = new System.Windows.Forms.GroupBox();
+            this.timerLabel = new System.Windows.Forms.Label();
+            this.endTimeBox = new System.Windows.Forms.GroupBox();
+            this.endTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.messageBox = new System.Windows.Forms.GroupBox();
+            this.message = new System.Windows.Forms.TextBox();
+            this.startStopButton = new System.Windows.Forms.Button();
+            this.clockBox.SuspendLayout();
+            this.timerBox.SuspendLayout();
+            this.endTimeBox.SuspendLayout();
+            this.messageBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // clock
+            // 
+            this.clock.Enabled = true;
+            this.clock.Interval = 1000;
+            this.clock.Tick += new System.EventHandler(this.Clock_Tick);
+            // 
+            // clockBox
+            // 
+            this.clockBox.Controls.Add(this.clockLabel);
+            this.clockBox.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.clockBox.Location = new System.Drawing.Point(12, 12);
+            this.clockBox.Name = "clockBox";
+            this.clockBox.Size = new System.Drawing.Size(371, 75);
+            this.clockBox.TabIndex = 0;
+            this.clockBox.TabStop = false;
+            this.clockBox.Text = "Current Time";
+            // 
+            // clockLabel
+            // 
+            this.clockLabel.AutoSize = true;
+            this.clockLabel.Font = new System.Drawing.Font("Consolas", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.clockLabel.Location = new System.Drawing.Point(6, 19);
+            this.clockLabel.Name = "clockLabel";
+            this.clockLabel.Size = new System.Drawing.Size(359, 37);
+            this.clockLabel.TabIndex = 0;
+            this.clockLabel.Text = "01/01/9999 12:34:56";
+            // 
+            // timerBox
+            // 
+            this.timerBox.Controls.Add(this.timerLabel);
+            this.timerBox.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.timerBox.Location = new System.Drawing.Point(12, 256);
+            this.timerBox.Name = "timerBox";
+            this.timerBox.Size = new System.Drawing.Size(227, 75);
+            this.timerBox.TabIndex = 1;
+            this.timerBox.TabStop = false;
+            this.timerBox.Text = "Countdown";
+            // 
+            // timerLabel
+            // 
+            this.timerLabel.AutoSize = true;
+            this.timerLabel.Font = new System.Drawing.Font("Consolas", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.timerLabel.Location = new System.Drawing.Point(6, 19);
+            this.timerLabel.Name = "timerLabel";
+            this.timerLabel.Size = new System.Drawing.Size(215, 37);
+            this.timerLabel.TabIndex = 0;
+            this.timerLabel.Text = "00:00:00:00";
+            // 
+            // endTimeBox
+            // 
+            this.endTimeBox.Controls.Add(this.endTimePicker);
+            this.endTimeBox.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.endTimeBox.Location = new System.Drawing.Point(12, 93);
+            this.endTimeBox.Name = "endTimeBox";
+            this.endTimeBox.Size = new System.Drawing.Size(371, 75);
+            this.endTimeBox.TabIndex = 2;
+            this.endTimeBox.TabStop = false;
+            this.endTimeBox.Text = "Countdown To:";
+            // 
+            // endTimePicker
+            // 
+            this.endTimePicker.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.endTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.endTimePicker.Location = new System.Drawing.Point(6, 22);
+            this.endTimePicker.Name = "endTimePicker";
+            this.endTimePicker.Size = new System.Drawing.Size(358, 32);
+            this.endTimePicker.TabIndex = 0;
+            this.endTimePicker.Value = new System.DateTime(2020, 12, 4, 0, 0, 0, 0);
+            // 
+            // messageBox
+            // 
+            this.messageBox.Controls.Add(this.message);
+            this.messageBox.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.messageBox.Location = new System.Drawing.Point(12, 175);
+            this.messageBox.Name = "messageBox";
+            this.messageBox.Size = new System.Drawing.Size(371, 75);
+            this.messageBox.TabIndex = 3;
+            this.messageBox.TabStop = false;
+            this.messageBox.Text = "Message";
+            // 
+            // message
+            // 
+            this.message.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.message.Location = new System.Drawing.Point(7, 23);
+            this.message.Name = "message";
+            this.message.Size = new System.Drawing.Size(357, 33);
+            this.message.TabIndex = 0;
+            this.message.Text = "Time Left:";
+            // 
+            // startStopButton
+            // 
+            this.startStopButton.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.startStopButton.Location = new System.Drawing.Point(245, 256);
+            this.startStopButton.Name = "startStopButton";
+            this.startStopButton.Size = new System.Drawing.Size(138, 75);
+            this.startStopButton.TabIndex = 4;
+            this.startStopButton.Text = "Start";
+            this.startStopButton.UseVisualStyleBackColor = true;
+            this.startStopButton.Click += new System.EventHandler(this.startStopButton_Click);
+            // 
+            // Timer_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 349);
+            this.Controls.Add(this.startStopButton);
+            this.Controls.Add(this.messageBox);
+            this.Controls.Add(this.endTimeBox);
+            this.Controls.Add(this.timerBox);
+            this.Controls.Add(this.clockBox);
+            this.Name = "Timer_Form";
+            this.Text = "Form1";
+            this.clockBox.ResumeLayout(false);
+            this.clockBox.PerformLayout();
+            this.timerBox.ResumeLayout(false);
+            this.timerBox.PerformLayout();
+            this.endTimeBox.ResumeLayout(false);
+            this.messageBox.ResumeLayout(false);
+            this.messageBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer clock;
+        private System.Windows.Forms.GroupBox clockBox;
+        private System.Windows.Forms.Label clockLabel;
+        private System.Windows.Forms.GroupBox timerBox;
+        private System.Windows.Forms.Label timerLabel;
+        private System.Windows.Forms.GroupBox endTimeBox;
+        private System.Windows.Forms.DateTimePicker endTimePicker;
+        private System.Windows.Forms.GroupBox messageBox;
+        private System.Windows.Forms.TextBox message;
+        private System.Windows.Forms.Button startStopButton;
+    }
+}
+
---
+++ OBS Timer/OBS Timer/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OBS_Timer
+{
+    public partial class Timer_Form : Form
+    {
+        private DateTime _currentTime;
+        private DateTime? _endTime;
+        private string _filePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\obs_timer_out.txt";
+
+        public Timer_Form()
+        {
+            InitializeComponent();
+            
+            endTimePicker.Format = DateTimePickerFormat.Custom;
+            endTimePicker.CustomFormat = "MM/dd/yyyy HH:mm:ss";
+
+            UpdateCurrentTime();
+            endTimePicker.Value = _currentTime;
+        }
+
+        private void Clock_Tick(object sender, EventArgs e)
+        {
+            UpdateCurrentTime();
+        }
+
+
+        private void UpdateCurrentTime()
+        {
+            _currentTime = GetTruncatedTime(DateTime.Now, TimeSpan.FromSeconds(1));
+            UpdateClock();
+            UpdateTimer();
+        }
+
+        private DateTime GetTruncatedTime(DateTime dateTime, TimeSpan timeSpan)
+        {
+            return dateTime.AddTicks(-(dateTime.Ticks % timeSpan.Ticks));
+        }
+
+        private void UpdateClock()
+        {
+            clockLabel.Text = _currentTime.ToString("MM/dd/yyyy HH:mm:ss");
+        }
+
+        private void UpdateTimer()
+        {
+            if (_endTime is null)
+            {
+                timerLabel.Text = "00:00:00:00";
+                return;
+            }
+
+            TimeSpan timeLeft = (TimeSpan)(_endTime - _currentTime);
+
+            if (timeLeft > TimeSpan.Zero)
+            {
+                string timerText = timeLeft.ToString(@"dd\:hh\:mm\:ss");
+                timerLabel.Text = timerText;
+
+                if (message.Text == "") File.WriteAllText(_filePath, timerText);
+                else File.WriteAllText(_filePath, $"{message.Text} {timerText}");
+            }
+
+            else _endTime = null;
+        }
+
+        private void startStopButton_Click(object sender, EventArgs e)
+        {
+            if (startStopButton.Text == "Start")
+            {
+                if (endTimePicker.Value > _currentTime)
+                {
+                    _endTime = endTimePicker.Value;
+                    UpdateTimer();
+                    startStopButton.Text = "Stop";
+                    endTimePicker.Enabled = false;
+                }
+
+                else MessageBox.Show("Countdown end time must be after current time.", "Invalid time input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            else
+            {
+                _endTime = null;
+                UpdateTimer();
+                startStopButton.Text = "Start";
+                endTimePicker.Enabled = true;
+            }
+        }
+    }
+}
